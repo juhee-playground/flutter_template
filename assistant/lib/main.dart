5db64ae0bed5d2c1172bec8c7d7f263f1f8f77ae
김철 <dmsghs92@gmail.com>
2024-06-08 21:23:13 +0900

    Set env

--- assistant/lib/main.dart
+++ assistant/lib/main.dart
@@ -5,8 +5,13 @@ import 'src/app.dart';
 
 import 'src/provider/counter.dart';
 import 'src/provider/setting.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
+  await FlutterConfig.loadEnvVariables();
+  // 사용법: FlutterConfig.get('FABRIC_ID')
+
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => Counter()),
